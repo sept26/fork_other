4a1700e68f5547bc74fca41ac05465516da5947d
PC-20181204WGTS\Administrator <wx3364@163.com>
2019-02-13 17:45:02 +0800

    add router

--- src/page/home/index.vue
+++ src/page/home/index.vue
@@ -1,15 +1,11 @@
 <template>
-  <div id="app">
-    <router-view/>
+  <div class="home">
+
   </div>
 </template>
-
 <script>
 export default {
-  name: 'App',
+
 };
 </script>
 
-<style>
-
-</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,30 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import HelloWorld from '@/components/HelloWorld';
+import App from '../App';
+import home from '../page/home';
+import item from '../page/item';
+import score from '../page/score';
 
 Vue.use(Router);
 
 const routes = [
   {
     path: '/',
-    name: 'HelloWorld',
-    component: HelloWorld,
+    component: App,
+    children: [
+      {
+        path: '',
+        component: home,
+      },
+      {
+        path: '/item',
+        component: item,
+      },
+      {
+        path: '/score',
+        component: score,
+      },
+    ],
   },
 ];
 
